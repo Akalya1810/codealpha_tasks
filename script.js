db9d0350de2608b7e3c3b3ef24abd712ae6c764c
Akalya1810 <akalyahas1877@gmail.com>
2025-05-07 21:37:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const task = input.value.trim();
+  if (task === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  li.onclick = () => li.style.textDecoration = "line-through";
+
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
 
